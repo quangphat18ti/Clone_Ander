bfd6471cdc9121bca5f509e69c529a69f4de2013
Nam G VU <namgivu@gmail.com>
2023-07-24 10:33:58 +0700

    :member_list defined & display 0th

--- reactjs_webapp/src/App.js
+++ reactjs_webapp/src/App.js
@@ -1,6 +1,13 @@
+let member_list = [
+  { 'index': '00', 'fullname': 'Nam G VU',  'gitlab email': 'namgivu@gmail.com',   'district': 10, 'birth': 1982, 'git': 'gitlab namgivu, github namgivu', },
+  { 'index': '99', 'fullname': 'Han Ng VU', 'gitlab email': 'rubyhanvu@gmail.com', 'district': 3,  'birth': 2016, 'git': null, },
+  //TODO dear member, please fill in your info here; keep fieldname col aligned, thanks
+]
+
 function App() {
   return (
     <>
+      <div>{JSON.stringify(member_list)}</div>
     </>
   );
 }
