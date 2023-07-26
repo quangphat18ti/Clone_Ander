eb3b0cb9b214886053bb75c1d40bd10bc3837d3a
KTra <kt7456158@gmail.com>
2023-07-26 11:55:04 +0700

    Khuyen add Delete button

--- reactjs_webapp/src/App.js
+++ reactjs_webapp/src/App.js
@@ -52,6 +52,7 @@ function App() {
 
                 <td>
                   <button type="button" className="btn btn-primary">Edit</button>
+                  <button type="button" className="btn btn-danger">Delete</button>
                 </td>
               </tr>
             )}
