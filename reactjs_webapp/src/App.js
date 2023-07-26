da7a4bb3cb4f9917a8344254e95bcd350e0e066c
Nam G VU <namgivu@gmail.com>
2023-07-26 15:33:17 +0700

    fix warning @ Each child in a list should have a unique "key" prop.

--- reactjs_webapp/src/App.js
+++ reactjs_webapp/src/App.js
@@ -38,17 +38,17 @@ function App() {
         <table className="table">
           <thead>
             <tr>
-              {col_header_list.map(h => <td>{h}</td> )}
+              {col_header_list.map(h => <td key={`header-${h}`} >{h}</td> )}
               <td>action</td>
             </tr>
           </thead>
 
           <tbody>
-            {member_list.map(m =>
-              <tr>
+            {member_list.map( (m,i) =>
+              <tr key={`member-${i}`} >
                 {/* value col(s) */}
                 {col_header_list.map(h =>
-                  <td>{m[h]}</td>
+                  <td key={`memberinfo-${i}-${h}`} >{m[h]}</td>
                 )}
 
                 {/* action col */}
