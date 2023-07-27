6a4ae225c6e3ad27fb449f2e630bc36915beac46
Nam G VU <namgivu@gmail.com>
2023-07-27 23:46:53 +0700

    demo bs4 nav-tab

--- reactjs_webapp/src/layout.js
+++ reactjs_webapp/src/layout.js
@@ -1,22 +1,20 @@
 const Layout = ()=> {
   return(
     <>
-      <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
-        <li className="nav-item mx-2" role="presentation">
-          <button className="nav-link active" id="home-tab" data-toggle="tab" data-target="#home" type="button" role="tab"  aria-selected="true">Home</button>
-        </li>
-        <li className="nav-item mx-2" role="presentation">
-          <button className="nav-link" id="profile-tab" data-toggle="tab" data-target="#profile" type="button" role="tab"  aria-selected="false">Profile</button>
-        </li>
-        <li className="nav-item mx-2" role="presentation">
-          <button className="nav-link" id="contact-tab" data-toggle="tab" data-target="#contact" type="button" role="tab"  aria-selected="false">Contact</button>
-        </li>
-      </ul>
+      <div className='container-fluid'>
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+            <button class="nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+            <button class="nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+          </div>
+        </nav>
 
-      <div className="tab-content" id="myTabContent">
-        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Home Page</div>
-        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Profile Page</div>
-        <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Contact Page</div>
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+        </div>
       </div>
     </>
   )
