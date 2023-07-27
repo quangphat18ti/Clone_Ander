ff0baa1a941d9b6a7d8b60bd4df5dcccb908bd50
Nam G VU <namgivu@gmail.com>
2023-07-27 23:50:03 +0700

    about 0th

--- reactjs_webapp/src/layout.js
+++ reactjs_webapp/src/layout.js
@@ -3,17 +3,17 @@ const Layout = ()=> {
     <>
       <div className='container-fluid'>
         <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-            <button class="nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-            <button class="nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button className="nav-link active" id="nav-home-tab"    data-toggle="tab" data-target="#nav-home"    type="button" role="tab" aria-controls="nav-home"    aria-selected="true">Home</button>
+            <button className="nav-link       " id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+            <button className="nav-link       " id="nav-about-tab"   data-toggle="tab" data-target="#nav-about" type="button" role="tab" aria-controls="nav-about" aria-selected="false">About</button>
           </div>
         </nav>
 
-        <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+        <div className="tab-content" id="nav-tabContent">
+          <div className="tab-pane fade show active" id="nav-home"    role="tabpanel" aria-labelledby="nav-home-tab"    >...</div>
+          <div className="tab-pane fade            " id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" >...</div>
+          <div className="tab-pane fade            " id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab"     >TODO about</div>
         </div>
       </div>
     </>
