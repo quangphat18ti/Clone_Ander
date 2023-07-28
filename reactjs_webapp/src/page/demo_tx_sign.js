78124de2b1f55a03064c1e8bbced060cf1d3a69a
phanquocky <20120017@student.hcmus.edu.vn>
2023-07-28 20:58:02 +0700

    html for the keypair page

--- reactjs_webapp/src/page/demo_tx_sign.js
+++ reactjs_webapp/src/page/demo_tx_sign.js
@@ -1,3 +1,5 @@
+import KeyPair from "./demotxsign/keypair"
+
 function DemoTxSign() {
   return (
     <>
@@ -12,7 +14,7 @@ function DemoTxSign() {
         </nav>
 
         <div className="tab-content" id="nav-tabContent">
-          <div className="tab-pane fade show active" id="nav-key"                 role="tabpanel" aria-labelledby="nav-key-tab">                  TODO key         </div>
+          <div className="tab-pane fade show active" id="nav-key"                 role="tabpanel" aria-labelledby="nav-key-tab">             <KeyPair />  </div>
           <div className="tab-pane fade            " id="nav-signature"           role="tabpanel" aria-labelledby="nav-block-tab">                TODO signature   </div>
           <div className="tab-pane fade            " id="nav-transaction"         role="tabpanel" aria-labelledby="nav-transaction-tab">          TODO transaction </div>
           <div className="tab-pane fade            " id="nav-blockchain--demotxsign"  role="tabpanel" aria-labelledby="nav-blockchain--demotxsign-tab">   TODO blockchain  </div>
