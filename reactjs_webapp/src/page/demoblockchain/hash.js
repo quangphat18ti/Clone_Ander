93d693dcaa38a14df3d7ee7feac3bc98384b3dd5
Heyesvz <taconghoang1103@gmail.com>
2023-07-28 21:38:38 +0700

    Hoang add hash screen

--- reactjs_webapp/src/page/demoblockchain/hash.js
+++ reactjs_webapp/src/page/demoblockchain/hash.js
@@ -1,7 +1,26 @@
 function Hash() {
     return (
         <>
-            <p>TODO hash</p>
+            <div className="container">
+                <h1>SHA256 Hash</h1>
+
+                <div className="alert alert-dark" role="alert">
+                    <form>
+                        <div className="form-group row">
+                            <label htmlFor="data" className="col-sm-2 col-form-label text-right"><strong>Data:</strong></label>
+                            <div className="col-sm-10">
+                                <textarea className="form-control" id="data" rows="10"></textarea>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="hash" className="col-sm-2 col-form-label text-right"><strong>Hash:</strong></label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="hash" disabled />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
