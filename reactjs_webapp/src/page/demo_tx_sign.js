87d33fdef652e9b0198e59a4b6323a05c83d6476
Nam G VU <namgivu@gmail.com>
2023-07-28 10:18:20 +0700

    simpler code @ singular cont.

--- reactjs_webapp/src/page/demo_tx_sign.js
+++ reactjs_webapp/src/page/demo_tx_sign.js
@@ -4,18 +4,18 @@ function DemoTxSign() {
       <div className='container-fluid mt-3'>
         <nav>
           <div className="nav nav-tabs justify-content-end" id="nav-tab" role="tablist">
-            <button className="nav-link active" id="nav-key-tab"               data-toggle="tab" data-target="#nav-key"               type="button" role="tab" aria-controls="nav-key"                 aria-selected="true" >Key</button>
-            <button className="nav-link       " id="nav-signatures-tab"         data-toggle="tab" data-target="#nav-signatures"         type="button" role="tab" aria-controls="nav-signatures"           aria-selected="false">Signature</button>
+            <button className="nav-link active" id="nav-key-tab"                data-toggle="tab" data-target="#nav-key"                type="button" role="tab" aria-controls="nav-key"                  aria-selected="true" >Key</button>
+            <button className="nav-link       " id="nav-signature-tab"          data-toggle="tab" data-target="#nav-signature"          type="button" role="tab" aria-controls="nav-signature"            aria-selected="false">Signature</button>
             <button className="nav-link       " id="nav-transaction-tab"        data-toggle="tab" data-target="#nav-transaction"        type="button" role="tab" aria-controls="nav-transaction"          aria-selected="false">Transaction</button>
             <button className="nav-link       " id="nav-tx-sign-blockchain-tab" data-toggle="tab" data-target="#nav-tx-sign-blockchain" type="button" role="tab" aria-controls="nav-tx-sign-blockchain"   aria-selected="false">Blockchain</button>
           </div>
         </nav>
 
         <div className="tab-content" id="nav-tabContent">
-          <div className="tab-pane fade show active" id="nav-key"                role="tabpanel" aria-labelledby="nav-key-tab">                 TODO key         </div>
-          <div className="tab-pane fade            " id="nav-signatures"          role="tabpanel" aria-labelledby="nav-block-tab">                TODO signatures   </div>
-          <div className="tab-pane fade            " id="nav-transaction"         role="tabpanel" aria-labelledby="nav-transaction-tab">          TODO transaction  </div>
-          <div className="tab-pane fade            " id="nav-tx-sign-blockchain"  role="tabpanel" aria-labelledby="nav-tx-sign-blockchain-tab">   TODO blockchain   </div>
+          <div className="tab-pane fade show active" id="nav-key"                 role="tabpanel" aria-labelledby="nav-key-tab">                  TODO key         </div>
+          <div className="tab-pane fade            " id="nav-signature"           role="tabpanel" aria-labelledby="nav-block-tab">                TODO signature   </div>
+          <div className="tab-pane fade            " id="nav-transaction"         role="tabpanel" aria-labelledby="nav-transaction-tab">          TODO transaction </div>
+          <div className="tab-pane fade            " id="nav-tx-sign-blockchain"  role="tabpanel" aria-labelledby="nav-tx-sign-blockchain-tab">   TODO blockchain  </div>
         </div>
       </div>
     </>
