b3bb0401688dc042e8fd9abf2880984b2af9c182
Tan P NGUYEN <21120028@student.hcmus.edu.vn>
2023-07-29 15:55:46 +0700

    create region for mine

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -1,5 +1,11 @@
 import CryptoJS from "crypto-js";
 
+const sha256_hash = (data) => {
+    return CryptoJS.SHA256(data);
+};
+
+
+// region mine
 /*!
  * Initial value for maximumNonce, view at
  * https://github.com/anders94/blockchain-demo/blob/master/public/javascripts/blockchain.js#L6C25-L32
@@ -18,10 +24,6 @@ else if (DIFFICULTY_MINOR === 1) { maximumNonce *= 8;  } // 0001 require 3 more 
 else if (DIFFICULTY_MINOR <= 3) { maximumNonce *= 4;  } // 0011 require 2 more 0 bits
 else if (DIFFICULTY_MINOR <= 7) { maximumNonce *= 2;  } // 0111 require 1 more 0 bit
 
-const sha256_hash = (data) => {
-    return CryptoJS.SHA256(data);
-};
-
 function mine(block, data) {
     for (let x = 0; x < maximumNonce; x++) {
         let message = block.toString() + x.toString() + data;
@@ -32,5 +34,6 @@ function mine(block, data) {
         }
     }
 }
+// end region mine
 
 export {sha256_hash, mine};
