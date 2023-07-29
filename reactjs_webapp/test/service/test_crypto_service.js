a336423111a499cb7074bc8f923313343d5f4354
Nam G VU <namgivu@gmail.com>
2023-07-29 16:28:19 +0700

    import order

--- reactjs_webapp/test/service/test_crypto_service.js
+++ reactjs_webapp/test/service/test_crypto_service.js
@@ -1,5 +1,7 @@
-let {sha256_hash} = require('../../src/service/crypto_service')
 const assert = require('assert')
+//
+let {sha256_hash} = require('../../src/service/crypto_service')
+
 
 let test_sha256_hash = () => {
     let h = sha256_hash('some data content')
