fc648663eec6ad998a8d35e616d89931e4814eea
Nam G VU <namgivu@gmail.com>
2023-07-29 16:06:56 +0700

    often for w/ varname i

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -14,7 +14,7 @@ const DIFFICULTY_MINOR = 15
 const DIFFICULTY_MAJOR = 4
 
 var maximumNonce = 8
-for (var x=0; x<DIFFICULTY_MAJOR; x++) {
+for (var i=0; i<DIFFICULTY_MAJOR; i++) {
     maximumNonce *= 16
 }
 
@@ -24,12 +24,12 @@ else if (DIFFICULTY_MINOR <=  3) { maximumNonce *= 4  } // 0011 require 2 more 0
 else if (DIFFICULTY_MINOR <=  7) { maximumNonce *= 2  } // 0111 require 1 more 0 bit
 
 function mine(block, data) {  //FIXME data la mot truong/field cua block roi ref. https://t.me/c/1725591141/169/774
-    for (let x = 0; x < maximumNonce; x++) {
-        let message = block.toString() + x.toString() + data
+    for (let i = 0; i < maximumNonce; i++) {
+        let message = block.toString() + i.toString() + data
         const zeroString = '0'.repeat(DIFFICULTY_MAJOR)
         let hash_message = sha256_hash(message).toString()
         if (hash_message.substring(0, DIFFICULTY_MAJOR) === zeroString) {
-            return { nonce: x, hash: hash_message }
+            return { nonce: i, hash: hash_message }
         }
     }
 }
