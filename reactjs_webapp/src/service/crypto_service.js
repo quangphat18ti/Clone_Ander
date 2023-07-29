906c22b5aa69f7354b72c56fff4134d38f6632d2
phanquocky <20120017@student.hcmus.edu.vn>
2023-07-29 18:30:33 +0700

    add ref for the sign, verify function @service/crypto_service.js

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -1,5 +1,9 @@
+let Buffer = require('buffer')
 let CryptoJS = require('crypto-js')
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
 
+//ref https://github.com/anders94/public-private-key-demo/blob/master/views/keys.pug
 function gen_keypair(privkey = null) {
   const EC = require('elliptic').ec
   const ec = new EC('secp256k1')
@@ -20,7 +24,6 @@ function gen_keypair(privkey = null) {
 const sha256_hash = (data) => {
     return CryptoJS.SHA256(data)
 }
-
 
 //region mine
 /*
@@ -54,4 +57,24 @@ function mine({blockNum, data}) {
 }
 //endregion mine
 
-module.exports = {sha256_hash, gen_keypair, mine, DIFFICULTY_MAJOR}
+//ref https://github.com/anders94/public-private-key-demo/blob/master/views/signatures.pug
+let verifyMessage = (msg2Verify, publicKey, signature) => {
+    let tmpKey
+    try      { tmpKey = ec.keyFromPublic(publicKey, 'hex') }
+    catch(e) { return false }
+    let binaryMessage = Buffer.Buffer.from(CryptoJS.SHA256(msg2Verify).toString(CryptoJS.enc.Hex))
+  
+    try      { let isVerified = tmpKey.verify(binaryMessage, signature) ; return isVerified }
+    catch(e) { return false }
+};  
+
+//ref https://github.com/anders94/public-private-key-demo/blob/master/views/signatures.pug
+let signMessage = (message, privateKey) => {
+  let keypair = ec.keyFromPrivate(privateKey) // ref. anders' code
+  let h = sha256_hash(message)
+  let binaryMessage = Buffer.Buffer.from(h.toString(CryptoJS.enc.Hex));
+  let hexSignature = Buffer.Buffer.from(keypair.sign(binaryMessage).toDER()).toString('hex')
+  return hexSignature
+}
+
+module.exports = {sha256_hash, gen_keypair, mine, DIFFICULTY_MAJOR, signMessage, verifyMessage}
