cb374ca5d66ebd29e8559b415d89a52dd32a1490
phanquocky <20120017@student.hcmus.edu.vn>
2023-07-29 14:09:28 +0700

    implement the logic of the keypair page

--- reactjs_webapp/src/page/demotxsign/keypair.js
+++ reactjs_webapp/src/page/demotxsign/keypair.js
@@ -1,5 +1,16 @@
+import { useEffect, useState }     from  'react'
+import { gen_keypair }  from  '../../service/crypto_service'
+import bigInt from'big-integer'
 
 function KeyPair() {
+  let [privkey, setPrivkey] = useState();
+  let [pubkey, setPubkey]   = useState();
+
+  useEffect(()=>{
+    let keypair = gen_keypair();
+    setPrivkey(keypair.privkey);
+    setPubkey(keypair.pubkey);
+  }, [])
   return(
     <>
       <div className="cointainer mt-3 mx-5">
@@ -10,16 +21,24 @@ function KeyPair() {
             <div className="form-group">
               <label htmlFor="privkey" className="control-label mt-2">Private Key</label>
               <div className="input-group">
-                <input type="text" className="form-control mb-2" id="privkey"/>
+                <input type="number" className="form-control mb-2" id="privkey" value={bigInt(privkey, 16).toString()} onChange={(e) => {
+                  let prevkey_new = bigInt(e.target.value).toString(16)
+                  setPrivkey(prevkey_new)
+                  setPubkey(gen_keypair(prevkey_new).pubkey)
+                }}/>
                 <span className="input-group-append">
-                  <button className="btn btn-secondary mb-2" >Random</button>
+                  <button className="btn btn-secondary mb-2" onClick={()=>{
+                    let keypair = gen_keypair()
+                    setPrivkey(keypair.privkey)
+                    setPubkey(keypair.pubkey)
+                  }}>Random</button>
                 </span>
               </div>
             </div>
 
-            <div className="form-group mb-5">label
+            <div className="form-group mb-5">
               <label htmlFor="pubkey" className="mt-2">Public Key</label>
-              <input className="form-control" id="pubkey" type="text" placeholder="Readonly input here..." readOnly={true} />
+              <input className="form-control" id="pubkey" type="text" readOnly={true} value={pubkey}/>
             </div>
           </div>
 
