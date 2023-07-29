5d69edefff0161b8bf47d8d987acc0ccc5ace74d
phanquocky <20120017@student.hcmus.edu.vn>
2023-07-29 12:11:11 +0700

    implement function gen_keypair()
    Change the return type of gen_keypair() function

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -1,1 +1,15 @@
 //TODO
+function gen_keypair(privkey = null) {
+  const EC = require('elliptic').ec
+  const ec = new EC('secp256k1')
+ 
+  let keypair = ec.genKeyPair();
+  if (privkey != null)
+    keypair = ec.keyFromPrivate(privkey)
+  return {
+            "privkey":    keypair.getPrivate('hex'),
+            "pubkey":     keypair.getPublic('hex')
+          }
+} 
+
+export {gen_keypair}
