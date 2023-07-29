03d58883cfdfce8138b730d7bbfd42223da3ca11
Nam G VU <namgivu@gmail.com>
2023-07-29 16:06:29 +0700

    FIXME data la mot truong/field cua block roi ref. https://t.me/c/1725591141/169/774

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -23,7 +23,7 @@ else if (DIFFICULTY_MINOR === 1) { maximumNonce *= 8  } // 0001 require 3 more 0
 else if (DIFFICULTY_MINOR <=  3) { maximumNonce *= 4  } // 0011 require 2 more 0 bits
 else if (DIFFICULTY_MINOR <=  7) { maximumNonce *= 2  } // 0111 require 1 more 0 bit
 
-function mine(block, data) {
+function mine(block, data) {  //FIXME data la mot truong/field cua block roi ref. https://t.me/c/1725591141/169/774
     for (let x = 0; x < maximumNonce; x++) {
         let message = block.toString() + x.toString() + data
         const zeroString = '0'.repeat(DIFFICULTY_MAJOR)
