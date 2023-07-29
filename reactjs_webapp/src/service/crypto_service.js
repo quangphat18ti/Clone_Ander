59b2a3154048026fe90de5c935b2579024ff38f1
Nam G VU <namgivu@gmail.com>
2023-07-29 16:02:38 +0700

    noneed ;

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -1,8 +1,8 @@
-import CryptoJS from "crypto-js";
+import CryptoJS from "crypto-js"
 
 const sha256_hash = (data) => {
-    return CryptoJS.SHA256(data);
-};
+    return CryptoJS.SHA256(data)
+}
 
 
 //region mine
@@ -10,29 +10,30 @@ const sha256_hash = (data) => {
 initial maximumNonce
 ref https://github.com/anders94/blockchain-demo/blob/master/public/javascripts/blockchain.js#L6C25-L32
 */
-const DIFFICULTY_MINOR = 15;
-const DIFFICULTY_MAJOR = 4;
+const DIFFICULTY_MINOR = 15
+const DIFFICULTY_MAJOR = 4
 
-var maximumNonce = 8;
+var maximumNonce = 8
 for (var x=0; x<DIFFICULTY_MAJOR; x++) {
-    maximumNonce *= 16;
+    maximumNonce *= 16
 }
 
-if      (DIFFICULTY_MINOR === 0) { maximumNonce *= 16; } // 0000 require 4 more 0 bits
-else if (DIFFICULTY_MINOR === 1) { maximumNonce *= 8;  } // 0001 require 3 more 0 bits
-else if (DIFFICULTY_MINOR <=  3) { maximumNonce *= 4;  } // 0011 require 2 more 0 bits
-else if (DIFFICULTY_MINOR <=  7) { maximumNonce *= 2;  } // 0111 require 1 more 0 bit
+if      (DIFFICULTY_MINOR === 0) { maximumNonce *= 16 } // 0000 require 4 more 0 bits
+else if (DIFFICULTY_MINOR === 1) { maximumNonce *= 8  } // 0001 require 3 more 0 bits
+else if (DIFFICULTY_MINOR <=  3) { maximumNonce *= 4  } // 0011 require 2 more 0 bits
+else if (DIFFICULTY_MINOR <=  7) { maximumNonce *= 2  } // 0111 require 1 more 0 bit
 
 function mine(block, data) {
     for (let x = 0; x < maximumNonce; x++) {
-        let message = block.toString() + x.toString() + data;
-        const zeroString = '0'.repeat(DIFFICULTY_MAJOR);
-        let hash_message = sha256_hash(message).toString();
+        let message = block.toString() + x.toString() + data
+        const zeroString = '0'.repeat(DIFFICULTY_MAJOR)
+        let hash_message = sha256_hash(message).toString()
         if (hash_message.substring(0, DIFFICULTY_MAJOR) === zeroString) {
-            return { nonce: x, hash: hash_message };
+            return { nonce: x, hash: hash_message }
         }
     }
 }
 //endregion mine
 
-export {sha256_hash, mine};
+
+export {sha256_hash, mine}
