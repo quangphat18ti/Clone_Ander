8a3be08149960c1729fd2d464b3790c49b913b1c
Tan P NGUYEN <21120028@student.hcmus.edu.vn>
2023-07-29 18:18:33 +0700

    para: block -> blockNum @mine() and create region mine

--- reactjs_webapp/src/service/crypto_service.js
+++ reactjs_webapp/src/service/crypto_service.js
@@ -4,18 +4,36 @@ const sha256_hash = (data) => {
     return CryptoJS.SHA256(data)
 }
 
-const DIFFICULTY = 4;
-var maximumNonce = 100000;
+
+//region mine
+/*
+initial maximumNonce
+ref https://github.com/anders94/blockchain-demo/blob/master/public/javascripts/blockchain.js#L6C25-L32
+*/
+let DIFFICULTY_MINOR = 15
+let DIFFICULTY_MAJOR = 4
+
+let maximumNonce = 8
+for (let i=0; i<DIFFICULTY_MAJOR; i++) {
+    maximumNonce *= 16
+}
+
+if      (DIFFICULTY_MINOR === 0) { maximumNonce *= 16 } // 0000 require 4 more 0 bits
+else if (DIFFICULTY_MINOR === 1) { maximumNonce *= 8  } // 0001 require 3 more 0 bits
+else if (DIFFICULTY_MINOR <=  3) { maximumNonce *= 4  } // 0011 require 2 more 0 bits
+else if (DIFFICULTY_MINOR <=  7) { maximumNonce *= 2  } // 0111 require 1 more 0 bit
+
+const zeroString = '0'.repeat(DIFFICULTY_MAJOR);
 
 function mine(blockNum, data) {
     for (let x = 0; x < maximumNonce; x++) {
-        let message = block.toString() + x.toString() + data;
-        const zeroString = '0'.repeat(DIFFICULTY);
+        let message = blockNum.toString() + x.toString() + data;
         let hash_message = sha256_hash(message).toString();
-        if (hash_message.substring(0, DIFFICULTY) === zeroString) {
+        if (hash_message.substring(0, DIFFICULTY_MAJOR) === zeroString) {
             return { nonce: x, hash: hash_message };
         }
     }
 }
+//endregion mine
 
 module.exports = {sha256_hash, mine}
