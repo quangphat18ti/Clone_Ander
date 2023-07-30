03759c80fdc72298f083e2352d71b1cc2c99850c
Nam G VU <namgivu@gmail.com>
2023-07-30 15:28:09 +0700

    explain code

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -4,12 +4,13 @@ import {mine} from "../../service/crypto_service"
 function BlockChain() {
   //region cook block_list data
   let block_list = [
-    {blockNum: '01', data: 'listoftx 01', prev: '...',  nonce: '53675',  'hash': '00001de0cc9d93dce583a5fa07bbd9a84cd8f35ff78aa2a8ea56aa5de6fca708', },
-    {blockNum: '02', data: 'listoftx 02', prev: 'TODO', nonce: '149193', 'hash': '0000c2bbcfaac7683bae4c9bbe9317d71b36f9cbd7194048200e9064589feffd', },
-    {blockNum: '03', data: 'listoftx 03', prev: 'TODO', nonce: '66452',  'hash': '0000711e68d600af7f06372f8a66130bb8c6b6b26eb8568e4ca1e022204ca2d8', },
+    //                                                  nonce: pre-computed value and set here
+    {blockNum: '01', data: 'listoftx 01', prev: '...',  nonce: '53675',  hash: 'TODO', },
+    {blockNum: '02', data: 'listoftx 02', prev: 'TODO', nonce: '149193', hash: 'TODO', },
+    {blockNum: '03', data: 'listoftx 03', prev: 'TODO', nonce: '66452',  hash: 'TODO', },
   ]
 
-  // set :prev fr current hash values
+  //region set :prev fr current hash values
   block_list = block_list.map( (b,i) => {
     let prev = i>0 ? block_list[i-1].hash : '...'
     return {
@@ -17,6 +18,7 @@ function BlockChain() {
       prev,
     }
   })
+  //endregion set :prev fr current hash values
 
   //region mine all blocks
   let do_remine = false
