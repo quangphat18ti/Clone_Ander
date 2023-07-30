8bb3c24a6a2566adda65c55bde1116e2ea673965
Nam G VU <namgivu@gmail.com>
2023-07-30 10:58:50 +0700

    tabpane blockchain 0th

---
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -0,0 +1,14 @@
+function BlockChain() {
+  return (
+    <>
+      {/* ref anders demo ref https://andersbrownworth.com/blockchain/blockchain */}
+      <h3>Blockchain</h3>
+
+      <div class="row row-horizon">
+        TODO
+      </div>
+    </>
+  )
+}
+
+export default BlockChain
