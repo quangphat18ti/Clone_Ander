74935afc8ed8643061d43d65eb30af2ed7b1e0e6
Nam G VU <namgivu@gmail.com>
2023-07-31 00:50:44 +0700

    region set :prev

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -11,22 +11,25 @@ function BlockChain() {
     {blockNum: '4', data: 'listoftx 04',               nonce: '30725',                                                                                                                                                        prev: '00006aa44d505d663ae3ff36180cba3f7fbca96f0449ef27b71a6fe3899ae238',  hash: '0000b7268be6dd274b320fb1b3992df81d82f900205bdd86a393c65cb58beaeb', },
   ]
 
-  //region set :prev fr current hash values
-  block_list = block_list.map( (b,i) => {
-    let prev = i>0 ? block_list[i-1].hash : '...'
-    return {...b, prev}
-  })
-  //endregion set :prev fr current hash values
+  //region set :prev
+  let do_set_prev = false
+  if (do_set_prev) {
+    block_list = block_list.map( (b,i) => {
+      let prev = i>0 ? block_list[i-1].hash : '...'
+      return {...b, prev}
+    })
+  }
+  //endregion set :prev
 
-  //region mine all blocks
-  let do_remine = false
-  if (do_remine===true) {
+  //region do_mine_again
+  let do_mine_again = false
+  if (do_mine_again===true) {
     block_list = block_list.map(b => {
       let {nonce, hash} = mine(b)
       return {...b, nonce, hash}
     })
   }
-  //endregion mine all blocks
+  //endregion do_mine_again
 
   //endregion cook block_list data
 
