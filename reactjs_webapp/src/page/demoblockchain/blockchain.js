e6bf6919f76259b8e79e3ffc798a07ab0be1b2f2
Nam G VU <namgivu@gmail.com>
2023-07-30 15:10:26 +0700

    mina all block by mine()

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -1,4 +1,5 @@
 import Block from "./block"
+import {mine} from "../../service/crypto_service"
 
 function BlockChain() {
   //region cook block_list data
@@ -7,6 +8,16 @@ function BlockChain() {
     {blockNum: '02', data: 'listoftx 02', prev: 'TODO', nonce: 'TODO', 'hash': 'TODO', },
     {blockNum: '03', data: 'listoftx 03', prev: 'TODO', nonce: 'TODO', 'hash': 'TODO', },
   ]
+
+  // mine all blocks
+  block_list = block_list.map(b => {
+    let {nonce, hash} = mine(b)
+    return {
+      ...b,
+      nonce,
+      hash,
+    }
+  })
   //endregion cook block_list data
 
   return (
