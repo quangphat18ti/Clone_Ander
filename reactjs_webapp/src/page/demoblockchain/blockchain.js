eb61f3610de0203a54a86a95d697ff96c5ee77ef
Nam G VU <namgivu@gmail.com>
2023-07-30 11:02:43 +0700

    render list of 3 + TODO 3+ onsameline

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -1,11 +1,16 @@
+import Block from "./block"
+
 function BlockChain() {
   return (
     <>
       {/* ref anders demo ref https://andersbrownworth.com/blockchain/blockchain */}
       <h3>Blockchain</h3>
 
-      <div class="row row-horizon">
-        TODO
+      <div className="row row-horizon">
+        <div className="col-4"><Block /></div>
+        <div className="col-4"><Block /></div>
+        <div className="col-4"><Block /></div>
+        {/*TODO make :Block list on same line*/}
       </div>
     </>
   )
