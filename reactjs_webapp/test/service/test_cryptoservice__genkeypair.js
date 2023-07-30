d39e10e2da989a0ffc4f919cd7315c6aacbd8381
Nam G VU <namgivu@gmail.com>
2023-07-30 08:51:03 +0700

    indent w/ 2-space

--- reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
+++ reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
@@ -4,15 +4,15 @@ let {gen_keypair} = require('../../src/service/crypto_service')
 
 
 let test_gen_keypair__tc00_create_brandnew = () => {
-    let keypair = gen_keypair()
-    assert(keypair.privkey, 'we must have .privkey defined')
-    assert(keypair.pubkey,  'we must have .pubkey defined')
+  let keypair = gen_keypair()
+  assert(keypair.privkey, 'we must have .privkey defined')
+  assert(keypair.pubkey,  'we must have .pubkey defined')
 }; test_gen_keypair__tc00_create_brandnew()
 
 let test_gen_keypair__tc01_get_pubkey_fr_privkey = () => {
-    let keypair = gen_keypair()
-    let privkey = keypair.privkey
+  let keypair = gen_keypair()
+  let privkey = keypair.privkey
 
-    let pubkey = gen_keypair(privkey)
-    assert(pubkey,  'we must have .pubkey defined')
+  let pubkey = gen_keypair(privkey)
+  assert(pubkey,  'we must have .pubkey defined')
 }; test_gen_keypair__tc01_get_pubkey_fr_privkey()
