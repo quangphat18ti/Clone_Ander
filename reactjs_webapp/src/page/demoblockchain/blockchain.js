c559accc36fe626c6fc20a451cf74a3094eeca80
Nam G VU <namgivu@gmail.com>
2023-07-30 17:28:47 +0700

    calling :Mine at :block tabpane and paste mined block result here @ block 2

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -6,6 +6,7 @@ function BlockChain() {
   let block_list = [
     // calling :Mine at :block tabpane and paste mined block result here
     {blockNum: '1', data: 'listoftx 01', prev: '...',  nonce: '31266',  hash: '0000f31922fa0289d6b90bd16eef0a4def6dd018887d0eef10055153247ec5b9', },
+    {blockNum: '2', data: 'listoftx 02',               nonce: '125252', prev: '0000f31922fa0289d6b90bd16eef0a4def6dd018887d0eef10055153247ec5b9',  hash: '00000cc3305145158e2e9b61d6eae170afc1f8ba915ccca95a93e35116f2ce12', },
   ]
 
   //region set :prev fr current hash values
