1d4710cfe887b29ca888e6e14c804f04744c13ca
Nam G VU <namgivu@gmail.com>
2023-07-30 08:39:19 +0700

    unittest @ tc01_get_pubkey_fr_privkey

--- reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
+++ reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
@@ -8,3 +8,11 @@ let test_gen_keypair__tc00_create_brandnew = () => {
     assert(keypair.privkey, 'we must have .privkey defined')
     assert(keypair.pubkey,  'we must have .pubkey defined')
 }; test_gen_keypair__tc00_create_brandnew()
+
+let test_gen_keypair__tc01_get_pubkey_fr_privkey = () => {
+    let keypair = gen_keypair()
+    let privkey = keypair.privkey
+
+    let pubkey = gen_keypair(privkey)
+    assert(pubkey,  'we must have .pubkey defined')
+}; test_gen_keypair__tc01_get_pubkey_fr_privkey()
