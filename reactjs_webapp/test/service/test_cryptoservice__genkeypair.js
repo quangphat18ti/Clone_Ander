faba9f1b6da2a8546c6270ea48dd5d24148a63e0
Nam G VU <namgivu@gmail.com>
2023-07-30 08:56:24 +0700

    unittest @ tc02_get_pubkey_fr_emptyprivkey ''

--- reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
+++ reactjs_webapp/test/service/test_cryptoservice__genkeypair.js
@@ -16,3 +16,18 @@ let test_gen_keypair__tc01_get_pubkey_fr_privkey = () => {
   let pubkey = gen_keypair(privkey)
   assert(pubkey,  'we must have .pubkey defined')
 }; test_gen_keypair__tc01_get_pubkey_fr_privkey()
+
+
+let test_gen_keypair__tc02_get_pubkey_fr_emptyprivkey = () => {
+  let empty_privkey_setby_inputhtmltag_1 = ''
+
+  let privkey
+
+  try {
+    privkey = empty_privkey_setby_inputhtmltag_1
+    let _ = gen_keypair(privkey)  // should get error > TypeError: Cannot read properties of null (reading 'toString')
+  } catch (e) {
+    assert(e.toString().includes("TypeError: Cannot read properties of null (reading 'toString')"))
+  }
+
+}; test_gen_keypair__tc02_get_pubkey_fr_emptyprivkey()
