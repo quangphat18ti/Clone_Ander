0e9e9937894e6e2fb160e19191ad4440e54ea0df
Nam G VU <namgivu@gmail.com>
2023-07-30 14:20:31 +0700

    render block_list w/ Block component @ cook block_list data

--- reactjs_webapp/src/page/demoblockchain/blockchain.js
+++ reactjs_webapp/src/page/demoblockchain/blockchain.js
@@ -1,6 +1,14 @@
 import Block from "./block"
 
 function BlockChain() {
+  //region cook block_list data
+  let block_list = [
+    {blockNum: '01', data: 'listoftx 01', prev: 'TODO', nonce: 'TODO', 'hash': 'TODO', },
+    {blockNum: '02', data: 'listoftx 02', prev: 'TODO', nonce: 'TODO', 'hash': 'TODO', },
+    {blockNum: '03', data: 'listoftx 03', prev: 'TODO', nonce: 'TODO', 'hash': 'TODO', },
+  ]
+  //endregion cook block_list data
+
   return (
     <>
       {/* ref anders demo ref https://andersbrownworth.com/blockchain/blockchain */}
