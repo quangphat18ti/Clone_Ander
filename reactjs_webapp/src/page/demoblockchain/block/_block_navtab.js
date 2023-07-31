7444ffd4124883e2599a6e73c0aa7835c63c745b
Nam G VU <namgivu@gmail.com>
2023-07-31 21:41:56 +0700

    todo tabpane content

--- reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
+++ reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
@@ -3,8 +3,6 @@ import Block from "./block/block_mock"
 function BlockNavtab() {
   return (
     <>
-      {/*TODO navtab 1) mock data , 2) sepolia data*/}
-
       <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
         <li className="nav-item" role="presentation">
           <button className="nav-link active" id="pills-blockmock-tab" data-toggle="pill" data-target="#pills-blockmock" type="button" role="tab" aria-controls="pills-blockmock" aria-selected="true">Mock Data</button>
@@ -13,9 +11,10 @@ function BlockNavtab() {
           <button className="nav-link" id="pills-blocksepolia-tab" data-toggle="pill" data-target="#pills-blocksepolia" type="button" role="tab" aria-controls="pills-blocksepolia" aria-selected="false">Mock Sepolia</button>
         </li>
       </ul>
+
       <div className="tab-content" id="pills-tabContent">
-        <div className="tab-pane fade show active" id="pills-blockmock" role="tabpanel" aria-labelledby="pills-blockmock-tab">...h</div>
-        <div className="tab-pane fade" id="pills-blocksepolia" role="tabpanel" aria-labelledby="pills-blocksepolia-tab">...p</div>
+        <div className="tab-pane fade show active" id="pills-blockmock"    role="tabpanel" aria-labelledby="pills-blockmock-tab"    >TODO blockmock</div>
+        <div className="tab-pane fade            " id="pills-blocksepolia" role="tabpanel" aria-labelledby="pills-blocksepolia-tab" >TODO blocksepolia</div>
       </div>      
     </>
   )
