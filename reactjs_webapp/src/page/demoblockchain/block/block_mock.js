d3425c12baca726b84d247d2b914437343906603
Nam G VU <namgivu@gmail.com>
2023-07-31 22:14:11 +0700

    navpill aligned w/ :block content @ padding tuned

--- reactjs_webapp/src/page/demoblockchain/block/block_mock.js
+++ reactjs_webapp/src/page/demoblockchain/block/block_mock.js
@@ -24,106 +24,104 @@ function BlockMock(props) {
     <>
       {/* ref anders demo ref https://andersbrownworth.com/blockchain/block */}
 
-      <div className="container">
-        { props.showTitle==='true' &&
-          <h3>Block</h3>
-        }
-        <div className={`
-               alert ${isMined ? "alert-success" : "alert-danger"}
-               px-3 pt-3 pb-0
-             `} role="alert" style={{ color: 'black' }}
-        >
-          <form>
-            {/* blockNum */}
-            <div className="form-group row">
-              <label htmlFor="block" className="col-sm-2 col-form-label text-right"><strong>Block</strong></label>
+      { props.showTitle==='true' &&
+        <h3>Block</h3>
+      }
+      <div className={`
+             alert ${isMined ? "alert-success" : "alert-danger"}
+             px-3 pt-3 pb-0
+           `} role="alert" style={{ color: 'black' }}
+      >
+        <form>
+          {/* blockNum */}
+          <div className="form-group row">
+            <label htmlFor="block" className="col-sm-2 col-form-label text-right"><strong>Block</strong></label>
 
-              <div className="col-sm-10">
-                <div className="input-group">
-                  <span className="input-group-text" id="basic-addon1">#</span>
-                  <input type="number" className="form-control" id="blockchainnumber"
-                         value={blockNum || ''}
-                         onChange={(e) => {setBlockNum(e.target.value)}}
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* nonce */}
-            <div className="form-group row">
-              <label htmlFor="nonce" className="col-sm-2 col-form-label text-right"><strong>Nonce</strong></label>
-
-              <div className="col-sm-10">
-                <input type="text" className="form-control" id="nonce"
-                       value={nonce || ''}
-                       onChange={(e) => {setNonce(e.target.value)}}
+            <div className="col-sm-10">
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon1">#</span>
+                <input type="number" className="form-control" id="blockchainnumber"
+                       value={blockNum || ''}
+                       onChange={(e) => {setBlockNum(e.target.value)}}
                 />
               </div>
             </div>
+          </div>
 
-            {/* data */}
+          {/* nonce */}
+          <div className="form-group row">
+            <label htmlFor="nonce" className="col-sm-2 col-form-label text-right"><strong>Nonce</strong></label>
+
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="nonce"
+                     value={nonce || ''}
+                     onChange={(e) => {setNonce(e.target.value)}}
+              />
+            </div>
+          </div>
+
+          {/* data */}
+          <div className="form-group row">
+            <label htmlFor="data" className="col-sm-2 col-form-label text-right"><strong>Data</strong></label>
+
+            <div className="col-sm-10">
+              <textarea
+                className="form-control" id="data" rows="10"
+                onChange={(e) => { setData(e.target.value)}}
+                value={data || ''}
+              ></textarea>
+            </div>
+          </div>
+
+          {/* prev */}
+          { props.showPrev==='true' &&
             <div className="form-group row">
-              <label htmlFor="data" className="col-sm-2 col-form-label text-right"><strong>Data</strong></label>
-
+              <label htmlFor="prev" className="col-sm-2 col-form-label text-right"><strong>Prev</strong></label>
               <div className="col-sm-10">
-                <textarea
-                  className="form-control" id="data" rows="10"
-                  onChange={(e) => { setData(e.target.value)}}
-                  value={data || ''}
-                ></textarea>
+                <input type="text" className="form-control" id="prev" disabled value={prev} />
               </div>
             </div>
+          }
 
-            {/* prev */}
-            { props.showPrev==='true' &&
-              <div className="form-group row">
-                <label htmlFor="prev" className="col-sm-2 col-form-label text-right"><strong>Prev</strong></label>
-                <div className="col-sm-10">
-                  <input type="text" className="form-control" id="prev" disabled value={prev} />
-                </div>
-              </div>
-            }
+          {/* hash */}
+          <div className="form-group row">
+            <label htmlFor="hash" className="col-sm-2 col-form-label text-right"><strong>Hash</strong></label>
+            <div className="col-sm-10">
+            <input type="text" className="form-control" id="hash" disabled value={hash} />
+            </div>
+          </div>
 
-            {/* hash */}
-            <div className="form-group row">
-              <label htmlFor="hash" className="col-sm-2 col-form-label text-right"><strong>Hash</strong></label>
-              <div className="col-sm-10">
-              <input type="text" className="form-control" id="hash" disabled value={hash} />
-              </div>
+          {/* mine */}
+          <div className="form-group row">
+            {/* loading spinner */} {/*TODO toggle spinner @ onClick :Mine btn - ref demo at https://t.me/c/1725591141/169/835 */}
+            <div className="col-sm-2">
+              <i className="icon-spinner icon-spin icon-large"></i>
             </div>
 
-            {/* mine */}
-            <div className="form-group row">
-              {/* loading spinner */} {/*TODO toggle spinner @ onClick :Mine btn - ref demo at https://t.me/c/1725591141/169/835 */}
-              <div className="col-sm-2">
-                <i className="icon-spinner icon-spin icon-large"></i>
-              </div>
-
-              {/* mine btn */}
-              <div className="col-sm-10">
-                <button id="blockMineButton" className="btn btn-primary" data-style="expand-right" type="button">
-                  <span className="ladda-label"
-                    onClick={(e) => {
-                      try {
-                        let new_nonce = mine({blockNum, data})
-                        if(new_nonce !== undefined)
-                          setNonce(new_nonce.nonce)
-                        else
-                          alert("Cannot find a nonce!")
-                      }
-                      catch (error) {
-                        console.log("Error = ", error)
-                        alert("Get error. Detail in Console")
-                      }
-                    }}
-                  >Mine</span>
-                </button>
-              </div>
-
+            {/* mine btn */}
+            <div className="col-sm-10">
+              <button id="blockMineButton" className="btn btn-primary" data-style="expand-right" type="button">
+                <span className="ladda-label"
+                  onClick={(e) => {
+                    try {
+                      let new_nonce = mine({blockNum, data})
+                      if(new_nonce !== undefined)
+                        setNonce(new_nonce.nonce)
+                      else
+                        alert("Cannot find a nonce!")
+                    }
+                    catch (error) {
+                      console.log("Error = ", error)
+                      alert("Get error. Detail in Console")
+                    }
+                  }}
+                >Mine</span>
+              </button>
             </div>
-          </form>
 
-        </div>
+          </div>
+        </form>
+
       </div>
     </>
   )
