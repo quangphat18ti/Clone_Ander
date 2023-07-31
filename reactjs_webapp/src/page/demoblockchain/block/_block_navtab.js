2019a4e983b03d63f53405eb80ccd69fe8f08421
Nam G VU <namgivu@gmail.com>
2023-07-31 21:35:35 +0700

    navpill vs navtab

--- reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
+++ reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
@@ -3,7 +3,34 @@ import Block from "./block/block_mock"
 function BlockNavtab() {
   return (
     <>
-      TODO navtab 1) mock data , 2) sepolia data
+      {/*TODO navtab 1) mock data , 2) sepolia data*/}
+
+      <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button className="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+        </li>
+      </ul>
+      <div className="tab-content" id="pills-tabContent">
+        <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...h</div>
+        <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...p</div>
+      </div>      
+
+      <div className='container-fluid mt-3'>
+        <nav className="d-flex">
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button className="nav-link active" id="nav-hash-tab"  data-toggle="tab" data-target="#nav-hash"  type="button" role="tab" aria-controls="nav-hash"  aria-selected="true" >BlockMock</button>
+            <button className="nav-link       " id="nav-block-tab" data-toggle="tab" data-target="#nav-block" type="button" role="tab" aria-controls="nav-block" aria-selected="false">BlockSepolia</button>
+          </div>
+        </nav>
+
+        <div className="tab-content" id="nav-tabContent">
+          <div className="tab-pane fade show active" id="nav-hash"  role="tabpanel" aria-labelledby="nav-hash-tab">  TODO BlockMock    </div>
+          <div className="tab-pane fade            " id="nav-block" role="tabpanel" aria-labelledby="nav-block-tab"> TODO BlockSepolia </div>
+        </div>
+      </div>
     </>
   )
 }
