c562945b4e88825430b4b2f130c59d388706fc22
Nam G VU <namgivu@gmail.com>
2023-07-31 22:03:47 +0700

    make navpill smaller cont.

--- reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
+++ reactjs_webapp/src/page/demoblockchain/block/_block_navtab.js
@@ -4,8 +4,8 @@ function BlockNavtab() {
   return (
     <>
       <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li className="nav-item" role="presentation"> <button className="py-1 px-2 nav-link active" id="pills-blockmock-tab"    data-toggle="pill" data-target="#pills-blockmock"    type="button" role="tab" aria-controls="pills-blockmock"    aria-selected="true"  >Mock Data</button> </li>
-        <li className="nav-item" role="presentation"> <button className="py-1 px-2 nav-link       " id="pills-blocksepolia-tab" data-toggle="pill" data-target="#pills-blocksepolia" type="button" role="tab" aria-controls="pills-blocksepolia" aria-selected="false" >Mock Sepolia</button> </li>
+        <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link active" id="pills-blockmock-tab"    data-toggle="pill" data-target="#pills-blockmock"    type="button" role="tab" aria-controls="pills-blockmock"    aria-selected="true"  >Mock Data</button> </li>
+        <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link       " id="pills-blocksepolia-tab" data-toggle="pill" data-target="#pills-blocksepolia" type="button" role="tab" aria-controls="pills-blocksepolia" aria-selected="false" >Mock Sepolia</button> </li>
       </ul>
 
       <div className="tab-content" id="pills-tabContent">
