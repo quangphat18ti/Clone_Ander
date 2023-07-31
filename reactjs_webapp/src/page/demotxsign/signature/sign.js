489285cd8ae54fb1aacc0f242247a1893cad9bc1
phanquocky <20120017@student.hcmus.edu.vn>
2023-07-31 17:34:52 +0700

    set state for the sign page @ demotxsign/signature/sign
    fix state
    
    
    abc

--- reactjs_webapp/src/page/demotxsign/signature/sign.js
+++ reactjs_webapp/src/page/demotxsign/signature/sign.js
@@ -1,15 +1,24 @@
+import {useState} from 'react'
+
 function Sign(){
+  let [message, setMessage]     = useState('')
+  let [privkey, setPrivkey]     = useState(localStorage.getItem('privkey'))
+  let [signature, setSignature] = useState('')
   return(
     <>
       <form>
         <div className="form-group">
           <label className="form-label" htmlFor="message--sign">Message</label>
-          <textarea className="form-control" id="message--sign" rows="5" ></textarea>
+          <textarea className="form-control" id="message--sign" rows="5" value={message} onChange = {(e) => {
+            setMessage(e.target.value)
+          }}></textarea>
         </div>
 
         <div className="form-group">
           <label className="form-label" htmlFor="privkey--sign">Private Key</label>
-          <input className="form-control" id="privkey--sign" type="number"/>
+          <input className="form-control" id="privkey--sign" type="number" value={privkey} onChange = {(e) => {
+            setPrivkey(e.target.value)
+          }}/>
         </div>
 
         <div className="form-group">
@@ -17,8 +26,8 @@ function Sign(){
         </div>
 
         <div className="form-group">
-          <label className="form-label" htmlFor="signature">Message Signature</label>
-          <input className="form-control" id="signature" type="text" readOnly={true}/>
+          <label className="form-label" htmlFor="signature" >Message Signature</label>
+          <input className="form-control" id="signature" type="text" readOnly={true} value={signature}/>
         </div>
       </form>
     </>
