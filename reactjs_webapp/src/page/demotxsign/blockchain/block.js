d19d07297d7af51744721379e2d5d940ba51fade
phanquocky <20120017@student.hcmus.edu.vn>
2023-08-02 00:37:47 +0700

    add blockchain logic @demotxsign/blockchain

--- reactjs_webapp/src/page/demotxsign/blockchain/block.js
+++ reactjs_webapp/src/page/demotxsign/blockchain/block.js
@@ -6,7 +6,7 @@ import { sha256_hash, ZERO_PREFIX } from '../../../service/crypto_service';
 function Block(props){
   let [blockNum, setBlockNum] = useState(props.blockNum? props.blockNum : 1  )
   let [nonce, setNonce]       = useState(props.nonce?    props.nonce    : '72608' )
-	let [award, setAward]				= useState('100.00')
+	let [award, setAward]				= useState(props.coinbasevalue? props.coinbasevalue: '100.00')
 	let [coinbase, setCoinbase] = useState(props.coinbase? props.coinbase : '')
 	let [tx, setTx]							= useState(props.tx? 			 props.tx				: [])
 	let [prev, setPrev]         = useState(props.prev?     props.prev     : '' )
@@ -15,8 +15,23 @@ function Block(props){
 	
 	const updateState = (hash) => { setIsMine(hash.startsWith(ZERO_PREFIX) ? 0 : 1)}
 
+	const handleTxChange = (index, tx_new)=> {
+		let tx_clone = [...tx]
+		tx_clone[index] = {...tx_clone[index], ...tx_new}
+		setTx(tx_clone)
+	}
+
 	useEffect(()=> {
-		console.log("change lbock")
+		setBlockNum(props.blockNum)
+		setNonce(props.nonce)
+		setAward(props.coinbasevalue)
+		setCoinbase(props.coinbase)
+		setTx(props.tx)
+		setPrev(props.prev)
+	})
+
+	useEffect(()=> {
+		console.log('tx', tx)
 		let message = '' + blockNum + nonce + award + coinbase
 		message = tx.reduce((msg, curr) => 
 			msg += '' + curr.value + curr.from + curr.to + curr.seq + curr.sig
@@ -27,7 +42,15 @@ function Block(props){
 		updateState(hash_new)
 	}, [blockNum, nonce, award, coinbase, tx, prev])
 
-	console.log('tx', tx)
+	let block = {
+		blockNum,
+		nonce,
+		coinbasevalue: award,
+		coinbase,
+		tx,
+		prev
+	}
+
 	return(
 		<>
 			<div className="container">
@@ -38,7 +61,11 @@ function Block(props){
 							<div className="col-sm-10">
 								<div className="input-group">
 									<span className="input-group-text">#</span>
-									<input type="number" className="form-control" id="blockchainnumber" value={blockNum} onChange= {(e)=> {setBlockNum(e.target.value)}}/>
+									<input type="number" className="form-control" id="blockchainnumber" value={blockNum} onChange= {(e)=> {
+										setBlockNum(e.target.value)
+										let block_new = {...block, blockNum: e.target.value}
+										props.updateChain(props.block_index, props.chain_index, block_new)
+									}}/>
 								</div>
 							</div>
 						</div>
@@ -46,7 +73,11 @@ function Block(props){
 						<div className="form-group row">
 							<label htmlFor="nonce" className="col-sm-2 col-form-label text-right">Nonce:</label>
 							<div className="col-sm-10">
-								<input type="text" className="form-control" id="nonce" value={nonce} onChange= {(e)=> setNonce(e.target.value)}/>
+								<input type="text" className="form-control" id="nonce" value={nonce} onChange= {(e)=>{
+									setNonce(e.target.value)
+									let block_new = {...block, nonce: e.target.value}
+									props.updateChain(props.block_index, props.chain_index, block_new)
+								}}/>
 							</div>
 						</div>
 
@@ -55,14 +86,22 @@ function Block(props){
 							<div className="col-sm">
 								<div className="input-group">
 									<span className="input-group-text">$</span>
-									<input type="text" className="form-control" id="coinbasevalue" value={award} onChange= {(e)=> setAward(e.target.value)}/>
+									<input type="text" className="form-control" id="coinbasevalue" value={award} onChange= {(e)=> {
+										setAward(e.target.value)
+										let block_new = {...block, coinbasevalue: e.target.value}
+										props.updateChain(props.block_index, props.chain_index, block_new)
+									}}/>
 									<span className="input-group-text">
 											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
 												<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
 											</svg>
 											<i className="bi bi-arrow-right"></i>
 									</span>
-									<input type="text" className="form-control" id="coinbaseto" value={coinbase} onChange= {(e) => setCoinbase(e.target.value)}/>
+									<input type="text" className="form-control" id="coinbaseto" value={coinbase} onChange= {(e) => {
+										setCoinbase(e.target.value)
+										let block_new = {...block, coinbase: e.target.value}
+										props.updateChain(props.block_index, props.chain_index, block_new)
+									}}/>
 								</div>
 							</div>
 						</div>
@@ -70,14 +109,18 @@ function Block(props){
 						<div className="form-group row">
 							<label htmlFor="coinbase" className="col-sm-2 col-form-label text-right">Tx:</label>
 							<div className="col-sm">
-								{	tx.map((data) => <Tx {...data}/>) }
+								{	tx.map((data, index) => <Tx key={index} {...data} handleTxchange={handleTxChange} index={index}/>) }
 							</div>
 						</div>
 
 						<div className="form-group row">
 							<label htmlFor="prev" className="col-sm-2 col-form-label text-right">Prev:</label>
 							<div className="col-sm-10">
-								<input type="text" className="form-control" id="prev" value={prev} onChange= {(e) => {setPrev(e.target.value)}}/>
+								<input type="text" className="form-control" id="prev" value={prev} onChange= {(e) => {
+									setPrev(e.target.value)
+									let block_new = {...block, prev: e.target.value}
+									props.updateChain(props.block_index, props.chain_index, block_new)
+								}}/>
 							</div>
 						</div>
 
--- reactjs_webapp/src/page/demotxsign/blockchain/tx.js
+++ reactjs_webapp/src/page/demotxsign/blockchain/tx.js
@@ -12,29 +12,43 @@ function Tx(props){
     let message = value + from + to + seq
     let verify = verifyMessage(message, from, sig)
     setTxState(verify? 0 : 1)
-
   }, [value, from, to, seq, sig])
 
   return(
     <>
       <div className="input-group">
         <span className="input-group-text">$</span>
-        <input type="text" className="form-control" id="txvalue" value={value} onChange= {(e) => {setValue(e.target.value)}}/>
+        <input type="text" className="form-control" id="txvalue" value={value} onChange= {(e) => {
+            setValue(e.target.value)
+            props.handleTxchange(props.index, {value: e.target.value})
+          }}/>
         <span className="input-group-text">From:</span>
-        <input type="text" className="form-control" id="txfrom" value={from} onChange= {(e) => {setFrom(e.target.value)}}/>
+        <input type="text" className="form-control" id="txfrom" value={from} onChange= {(e) => {
+            setFrom(e.target.value)
+            props.handleTxchange(props.index, {from: e.target.value})
+          }}/>
         <span className="input-group-text">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
           </svg>
           <i className="bi bi-arrow-right"></i>
         </span>
-        <input type="text" className="form-control" id="txto" value={to} onChange={(e) => {setTo(e.target.value)}}/>
+        <input type="text" className="form-control" id="txto" value={to} onChange={(e) => {
+            setTo(e.target.value)
+            props.handleTxchange(props.index, {to: e.target.value})
+          }}/>
       </div>
       <div className="input-group">
         <span className="input-group-text">Seq:</span>
-        <input type="text" className="form-control col-sm-2" id="seq" value={seq} onChange={(e) => {setSeq(e.target.value)}}/>
+        <input type="text" className="form-control col-sm-2" id="seq" value={seq} onChange={(e) => {
+            setSeq(e.target.value)
+            props.handleTxchange(props.index, {seq: e.target.value})
+          }}/>
         <span className="input-group-text">Sig:</span>
-        <input type="text" className={`form-control ${txState == 0 ? '' : 'text-danger'}`} id="sig" value={sig} onChange={(e) => {setSig(e.target.value)}}/>
+        <input type="text" className={`form-control ${txState === 0 ? '' : 'text-danger'}`} id="sig" value={sig} onChange={(e) => {
+            setSig(e.target.value)
+            props.handleTxchange(props.index, {sig: e.target.value})
+          }}/>
       </div>
       <br/>
     </>
