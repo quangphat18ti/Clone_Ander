8297d17b3afc6474d0f9de782954f65576a7f853
Nam G VU <namgivu@gmail.com>
2023-08-02 13:19:36 +0700

    test Phat code 0th

---
+++ reactjs_webapp/zsandbox/phat_2__try_endpoint__test.js
@@ -0,0 +1,8 @@
+let assert = require('assert')
+//
+let {getNewestBlockHexNumber} = require("./phat_2__try_endpoint")
+
+let run = async ()=>{
+  let r = await getNewestBlockHexNumber()
+  assert(r)
+}; run()
