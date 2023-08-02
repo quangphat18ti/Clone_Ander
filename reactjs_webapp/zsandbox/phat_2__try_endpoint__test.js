e7e514b3b67e55933ed29ccb3a447dc948105c21
Nam G VU <namgivu@gmail.com>
2023-08-02 14:11:11 +0700

    fixed @ it just a typo of startsWith()

--- reactjs_webapp/zsandbox/phat_2__try_endpoint__test.js
+++ reactjs_webapp/zsandbox/phat_2__try_endpoint__test.js
@@ -5,5 +5,5 @@ let {getNewestBlockHexNumber} = require("./phat_2__try_endpoint")
 let run = async ()=>{
   let r = await getNewestBlockHexNumber()
   assert(r)
-  assert(r.toString().startWith('0x'))  //TODO how to getridof toString() convert?
+  assert(r.startsWith('0x'))
 }; run()
