0ded12229ec0ff5aa904d36670b72982b053fbaf
Nam G VU <namgivu@gmail.com>
2023-08-02 15:34:19 +0700

    shorter varname :blockObj

--- reactjs_webapp/src/page/demoblockchain/block/block_sepolia.js
+++ reactjs_webapp/src/page/demoblockchain/block/block_sepolia.js
@@ -2,13 +2,13 @@ import {useEffect, useState} from "react"
 import {getBlockInfoByBlockHexNumber, getNewestBlockHexNumber} from "../../../service/etherscan_service/_"
 
 function BlockSepolia() {
-  let [block_obj, set_block_obj] = useState()
+  let [blockObj, set_blockObj] = useState()
 
   useEffect( () => {
     let get_latest_blocknum = async ()=>{
       let bno = await getNewestBlockHexNumber()
       let b   = await getBlockInfoByBlockHexNumber(bno)
-      set_block_obj(b)
+      set_blockObj(b)
     }; get_latest_blocknum()  //NOTE must wrap code in an inner function to run w/ async; dont use useEffect( https://devtrium.com/posts/async-functions-useeffectasync()=>{} )  ref.
   }, [])
 
@@ -16,14 +16,14 @@ function BlockSepolia() {
     <>
       <h3>Block Sepolia</h3>
       <pre>
-        <br/>Prev         {block_obj.parentHash}
-        <br/>Block Number {block_obj.number}
-        <br/>Nonce        {block_obj.nonce}
-        <br/>Hash         {block_obj.hash}
+        <br/>Prev         {blockObj.parentHash}
+        <br/>Block Number {blockObj.number}
+        <br/>Nonce        {blockObj.nonce}
+        <br/>Hash         {blockObj.hash}
       </pre>
 
       <hr/>
-      <div>{JSON.stringify(block_obj)}</div>
+      <div>{JSON.stringify(blockObj)}</div>
     </>
   )
 }
