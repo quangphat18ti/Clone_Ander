f1a2ac0dcff054fdc8821d0fb12777f39272ae69
Nam G VU <namgivu@gmail.com>
2023-08-02 15:25:03 +0700

    onpageload to call endpoint to get :blockobj

--- reactjs_webapp/src/page/demoblockchain/block/block_sepolia.js
+++ reactjs_webapp/src/page/demoblockchain/block/block_sepolia.js
@@ -1,7 +1,19 @@
+import {useEffect, useState} from "react"
+// import {getBlockInfoByBlockHexNumber, getNewestBlockHexNumber} from "../../../service/etherscan_service/_"
+
 function BlockSepolia() {
+  let [block_obj, set_block_obj] = useState()
+
+  useEffect( () => {
+    // let bno = await getNewestBlockHexNumber()
+    // let b   = await getBlockInfoByBlockHexNumber(bno)
+    // set_block_obj(b)
+  }, [])
+
   return (
     <>
-      <h3>TODO BlockSepolia</h3>
+      <h3>Block Sepolia</h3>
+      <div>{JSON.stringify(block_obj)}</div>
     </>
   )
 }
