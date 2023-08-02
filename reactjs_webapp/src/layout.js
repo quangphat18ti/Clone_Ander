b9d92187daab7ed325210713d4b795ecbad1429f
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-08-03 01:48:26 +0700

    add Demo-Wallet tabpanel

--- reactjs_webapp/src/layout.js
+++ reactjs_webapp/src/layout.js
@@ -14,6 +14,7 @@ const Layout = ()=> {
             <button className="nav-link active" id="nav-demoblockchain-tab" data-toggle="tab" data-target="#nav-demoblockchain" type="button" role="tab" aria-controls="nav-demoblockchain" aria-selected="true" >Demo Blockchain</button>
             <button className="nav-link       " id="nav-demotxsign-tab"     data-toggle="tab" data-target="#nav-demotxsign"     type="button" role="tab" aria-controls="nav-demotxsign"     aria-selected="false">Demo Tx Sign</button>
             <button className="nav-link       " id="nav-about-tab"          data-toggle="tab" data-target="#nav-about"          type="button" role="tab" aria-controls="nav-about"          aria-selected="false">About</button>
+            <button className="nav-link       " id="nav-demowallet-tab"     data-toggle="tab" data-target="#nav-demowallet"     type="button" role="tab" aria-controls="nav-demowallet"     aria-selected="false">Demo Wallet</button>
             <ConnectWeb3Wallet />
           </div>
         </nav>
@@ -22,6 +23,7 @@ const Layout = ()=> {
           <div className="tab-pane fade show active" id="nav-demoblockchain" role="tabpanel" aria-labelledby="nav-demoblockchain-tab">  <DemoBlockchain/> </div>
           <div className="tab-pane fade            " id="nav-demotxsign"     role="tabpanel" aria-labelledby="nav-demotxsign-tab">      <DemoTxSign/>     </div>
           <div className="tab-pane fade            " id="nav-about"          role="tabpanel" aria-labelledby="nav-about-tab">           <About/>          </div>
+          <div className="tab-pane fade            " id="nav-demowallet"     role="tabpanel" aria-labelledby="nav-demowallet-tab">      @TODO Demo Wallet </div>
         </div>
       </div>
     </>
