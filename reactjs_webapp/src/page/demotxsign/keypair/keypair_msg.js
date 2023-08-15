32f873f98a4f1262501d7e6c37cab9818d14ab3c
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-08-15 09:25:47 +0700

    ft/key finish:  gen key_pair from message

--- reactjs_webapp/src/page/demotxsign/keypair/keypair_msg.js
+++ reactjs_webapp/src/page/demotxsign/keypair/keypair_msg.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+import { sha256_hash, gen_keypair } from "../../../service/crypto_service"
+
 export default function KeypairMessage() {
+  const [message, set__message] = useState('')
+  const [privateKey, set__privateKey] = useState('')
+  const [publicKey, set__publicKey] = useState('')
+
+  useEffect(()=> {
+    set__privateKey(sha256_hash('' + message).toString())
+  }, [message])
+
+  useEffect(()=> {
+    set__publicKey(privateKey ? gen_keypair(privateKey).pubkey : '')
+  }, [privateKey])
+
   return (
     <>
-      <h2> @TODO </h2>
+      <form>
+        <div className="form-group mx-4">
+          <label className="form-message" htmlFor="message"> <strong>Message</strong></label>
+          <input className="form-control" id     ="message" type="text" placeholder="Enter your message to generate private key" value={message || ''} 
+                onChange = {(e) => {set__message(e.target.value)}}
+          />
+        </div>
+
+        <div className="form-group mx-4">
+          <label className="form-message" htmlFor="private-key"> <strong>Private Key</strong></label>
+          <input className="form-control" id     ="private-key" type="text" disabled value={privateKey || ''}/>
+          <small className="form-text text-muted"> 
+            Private Key is SHA256(message) 
+          </small>
+        </div>
+
+        <div className="form-group mx-4">
+          <label className="form-message" htmlFor="public-key"> <strong>Public Key</strong></label>
+          <input className="form-control" id     ="public-key" type="text" disabled value={publicKey || ''}  />
+        </div>
+      </form>
     </>
   )
 }
