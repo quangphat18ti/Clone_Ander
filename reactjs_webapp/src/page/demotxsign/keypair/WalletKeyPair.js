fc241f73fbdce955672375327936a223efee62aa
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-08-15 11:42:46 +0700

    Create new tab WalletKeyPair

--- reactjs_webapp/src/page/demotxsign/keypair/WalletKeyPair.js
+++ reactjs_webapp/src/page/demotxsign/keypair/WalletKeyPair.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { sha256_hash, gen_keypair } from "../../../service/crypto_service"
 
-export default function KeypairMessage() {
+export default function WalletKeyPair() {
   const [message, set__message] = useState('')
   const [privateKey, set__privateKey] = useState('')
   const [publicKey, set__publicKey] = useState('')
@@ -18,8 +18,8 @@ export default function KeypairMessage() {
     <>
       <form>
         <div className="form-group mx-4 mt-3">
-          <label className="form-message" htmlFor="message"> <strong>Key Text</strong></label>
-          <input className="form-control" id     ="message" type="text" placeholder="Enter your key text to generate private key" value={message || ''} 
+          <label className="form-message" htmlFor="message"> <strong>Your Wallet Name</strong></label>
+          <input className="form-control" id     ="message" type="text" placeholder="Enter your Wallet name to generate private key" value={message || ''} 
                 onChange = {(e) => {set__message(e.target.value)}}
           />
         </div>
@@ -27,9 +27,9 @@ export default function KeypairMessage() {
         <div className="form-group mx-4">
           <label className="form-message" htmlFor="private-key"> <strong>Private Key</strong></label>
           <input className="form-control" id     ="private-key" type="text" disabled value={privateKey || ''}/>
-          <small className="form-text text-muted"> 
-            Private Key is SHA256(Key Text) 
-          </small>
+          {/* <small className="form-text text-muted"> 
+            Private Key is SHA256(message) 
+          </small> */}
         </div>
 
         <div className="form-group mx-4">
