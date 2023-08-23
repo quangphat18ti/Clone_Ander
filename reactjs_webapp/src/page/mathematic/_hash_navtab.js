1a85c681dbd14e9adf4eb4e8f77dc117fa921152
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-08-23 18:32:51 +0700

    Active nav-pill in _hash_navtab page

--- reactjs_webapp/src/page/mathematic/_hash_navtab.js
+++ reactjs_webapp/src/page/mathematic/_hash_navtab.js
@@ -9,7 +9,7 @@ export default function HashNavTab() {
       {/* ref anders demo ref https://andersbrownworth.com/blockchain/hash */}
       <div className="container-fluid">
         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-          <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link       " id="pills-whatIsSHA256-tab" data-toggle="pill" data-target="#pills-whatIsSHA256"  type="button" role="tab" aria-controls="pills-whatIsSHA256" aria-selected="false" >What is SHA256</button> </li>
+          <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link active" id="pills-whatIsSHA256-tab" data-toggle="pill" data-target="#pills-whatIsSHA256"  type="button" role="tab" aria-controls="pills-whatIsSHA256" aria-selected="false" >What is SHA256</button> </li>
           <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link       " id="pills-Applications-tab" data-toggle="pill" data-target="#pills-Applications"  type="button" role="tab" aria-controls="pills-Applications" aria-selected="false" >Applications</button> </li>
           <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link       " id="pills-6ProperCHF-tab"   data-toggle="pill" data-target="#pills-6ProperCHF"    type="button" role="tab" aria-controls="pills-6ProperCHF"   aria-selected="false" >6 Properties of CHF</button> </li>
           <li className="nav-item" role="presentation"> <button className="py-1 px-2 small  nav-link       " id="pills-mathematic-tab"   data-toggle="pill" data-target="#pills-mathematic"    type="button" role="tab" aria-controls="pills-mathematic"   aria-selected="false" >Mathematic of SHA256</button> </li>
